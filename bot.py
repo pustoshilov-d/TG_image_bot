f1a9a86d5be9b641f23dc8d0bca9bb7115aaa52d
root <root@DESKTOP-8Q6DBAB.localdomain>
2022-11-03 18:53:37 +0300

    init

--- bot.py
+++ bot.py
@@ -145,20 +145,20 @@ def main():
 
     dp.add_error_handler(error)
 
-    # if IS_PROD:
-    #     updater.start_webhook(listen="0.0.0.0",
-    #                           port=PORT,
-    #                           url_path=TOKEN)
-    #     # updater.bot.set_webhook(url=settings.WEBHOOK_URL)
-    #     updater.bot.set_webhook(APP_NAME + TOKEN)
-    # else:
-    #     updater.start_polling()
-    
-    updater.start_webhook(listen="0.0.0.0",
-                            port=PORT,
-                            url_path=TOKEN)
-    updater.bot.set_webhook(APP_NAME + TOKEN)
-    # updater.start_polling()
+    if IS_PROD:
+        updater.start_webhook(listen="0.0.0.0",
+                              port=PORT,
+                              url_path=TOKEN)
+        # updater.bot.set_webhook(url=settings.WEBHOOK_URL)
+        updater.bot.set_webhook(APP_NAME + TOKEN)
+    else:
+        updater.start_polling()
+
+    # updater.start_webhook(listen="0.0.0.0",
+    #                         port=PORT,
+    #                         url_path=TOKEN)
+    # updater.bot.set_webhook(APP_NAME + TOKEN)
+    # # updater.start_polling()
 
     updater.idle()
 
