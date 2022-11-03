d1255b05464d918e36d1e60dbb9da1cd933b4e51
root <root@DESKTOP-8Q6DBAB.localdomain>
2022-11-03 22:32:16 +0300

    init

--- bot.py
+++ bot.py
@@ -4,27 +4,32 @@
 
 import logging
 from pathlib import PosixPath
-from telegram import Update
 import uuid
 import os
 import glob
 import urllib.request
 import random
 
-from telegram.ext import CommandHandler, MessageHandler, Application
-from telegram.ext.filters import PHOTO, TEXT, StatusUpdate
-import telegram
+from telegram.ext import CommandHandler, MessageHandler, Application, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram import Update
+
 
 PHOTOS_DIR = "photos"
 CHATS_DB = "chats.txt"
 IS_PROD = bool(os.environ.get('IS_PROD', 'False'))
-print('IS_PROD', IS_PROD)
+IS_PROD = False
+
 APP_NAME = str(os.environ.get(
     'APP_NAME', "tg-image-bot"))
 CHANCE = float(os.environ.get('CHANCE', '0.6'))
 TOKEN = str(os.environ.get(
-    'TOKEN', ""))
+    'TOKEN', "5483400723:AAE7qPx7GVPoxIjR30YkkZuy5VaqNy1mYns"))
+TOKEN = "5483400723:AAE7qPx7GVPoxIjR30YkkZuy5VaqNy1mYns"
+
 PORT = int(os.environ.get('PORT', '8443'))
+
+print('IS_PROD', IS_PROD)
+print('TOKEN', TOKEN)
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -33,7 +38,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 
-def echo(update: TEXT, context):
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     url = update.message.text
     print(url)
     if str(url).startswith("http"):
@@ -41,13 +46,13 @@ def echo(update: TEXT, context):
 
         try:
             urllib.request.urlretrieve(url, photo_path)
-            update.message.reply_photo(open(photo_path, 'rb'))
-            update.message.reply_text("Изображение принято.")
+            await update.message.reply_photo(open(photo_path, 'rb'))
+            await update.message.reply_text("Изображение принято.")
         except Exception as e:
             print('image from web is bad', e)
-            update.message.reply_text("Изображение не принято.")
+            await update.message.reply_text("Изображение не принято.")
 
-    update.message.reply_text(
+    await update.message.reply_text(
         """
 Чтобы сохранить изображение в базе, отправь мне его или ссылку на него, которая начинается с \"http\".
 /test для единоразовой отправки во все чаты, /clear_db для очистки базы.
@@ -55,7 +60,7 @@ def echo(update: TEXT, context):
     )
 
 
-def added(update: StatusUpdate.NEW_CHAT_MEMBERS, context):
+async def added(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Echo the user message."""
     print('added')
     chat = str(update.message.chat.id)
@@ -71,24 +76,21 @@ def added(update: StatusUpdate.NEW_CHAT_MEMBERS, context):
         print('Not added')
 
 
-def photo(update: Update, context):
-    """Stores the photo and asks for a location."""
-    user = update.message.from_user
-
-    photo_file = update.message.photo[-1].get_file()
+async def photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    photo_file = await update.message.photo[-1].get_file()
     photo_path = PosixPath('photos/'+str(uuid.uuid4().fields[-1])+".jpg")
-    photo_file.download(photo_path)
+    await photo_file.download(photo_path)
 
-    update.message.reply_photo(open(photo_path, 'rb'))
-    update.message.reply_text("Изображение принято.")
+    await update.message.reply_photo(open(photo_path, 'rb'))
+    await update.message.reply_text("Изображение принято.")
 
 
-def error(update, context):
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
-def clear_photo_dir(update=None, context=None):
+async def clear_photo_dir(update: Update = None, context: ContextTypes.DEFAULT_TYPE = None) -> None:
     photos = glob.glob("photos/*")
     for photo in photos:
         print(photo)
@@ -96,18 +98,17 @@ def clear_photo_dir(update=None, context=None):
 
     print('Done')
     if update:
-        update.message.reply_text("База очищена.")
+        await update.message.reply_text("База очищена.")
 
 
-def send_photos(update=None, context=None, rand=True):
+async def send_photos(update: Update = None, context: ContextTypes.DEFAULT_TYPE = None, rand=True) -> None:
     if rand and random.uniform(0, 1) > CHANCE:
         print('CHANCE falls')
         if update:
-            update.message.reply_text("Попробуй ещё раз.")
+            await update.message.reply_text("Попробуй ещё раз.")
         return
 
     print('CHANCE wins')
-    bot = Application.builder().token('TOKEN').build().bot
 
     chats = open('chats.txt').readlines()
     chats = [int(chat.strip()) for chat in chats if chat != ""]
@@ -117,32 +118,38 @@ def send_photos(update=None, context=None, rand=True):
     if photos == []:
         print("no images")
         if update:
-            update.message.reply_text("Нет изображений в базе")
+            await update.message.reply_text("Нет изображений в базе")
     chats_good = []
     for chat in chats:
         try:
             photo = random.choice(photos)
-            bot.send_photo(chat, open(photo, 'rb'))
+            if context:
+                await context.bot.send_photo(chat, open(photo, 'rb'))
+            else:
+                bot = Application.builder().token(TOKEN).build().bot
+                await bot.send_photo(chat, open(photo, 'rb'))
+
             chats_good.append(chat)
             print('chat good', chat)
         except Exception as e:
             print('chat bad', chat, e)
     if update:
         chats_good = [str(chat) for chat in chats_good]
-        update.message.reply_text(
+        await update.message.reply_text(
             "Изображения отправлены в " + ", ".join(chats_good))
 
 
 def main():
-    application = Application.builder().token('TOKEN').build()
-    updater = application.updater
+
+    application = Application.builder().token(TOKEN).build()
+
     application.add_handler(CommandHandler("test", send_photos))
     application.add_handler(CommandHandler("clear_db", clear_photo_dir))
 
-    application.add_handler(MessageHandler(TEXT, echo))
-    application.add_handler(MessageHandler(PHOTO, photo))
+    application.add_handler(MessageHandler(filters.TEXT, echo))
+    application.add_handler(MessageHandler(filters.PHOTO, photo))
     application.add_handler(MessageHandler(
-        StatusUpdate.NEW_CHAT_MEMBERS, added))
+        filters.StatusUpdate.NEW_CHAT_MEMBERS, added))
 
     application.add_error_handler(error)
 
@@ -154,9 +161,7 @@ def main():
             webhook_url=f"https://{APP_NAME}.herokuapp.com/" + TOKEN
         )
     else:
-        updater.start_polling()
-
-    updater.idle()
+        application.run_polling()
 
 
 if __name__ == '__main__':
